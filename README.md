8a801f48ff20bcee7e82affefcb0bfdec434a79e
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-02-04 15:36:48 +0000

    linked html to js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # week3-ac1
+
+HOW TO LINK A HTML FILE TO .JS
+
+-linking an external HS filed to an existing HTML 
+-link tag: <script src="./script.js" > </script>
---
+++ Unsolved/script.js
@@ -0,0 +1,6 @@
+// Open up the console to see this log
+console.log("Your external JavaScript file is linked 🎉");
+
+console.log ( 'has this worked???');
+
+console.log ('success!!!');
